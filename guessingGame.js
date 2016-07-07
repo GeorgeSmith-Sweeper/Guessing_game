2b48afbe93cf25f9c56fa3c9bb1b2c71c8ec6c62
George Smith-Sweeper <george.smsweeper@gmail.com>
2016-07-07 13:21:46 -0400

    added lower of higher conditionals

--- guessingGame.js
+++ guessingGame.js
@@ -1,8 +1,7 @@
   /* **** Global Variables **** */
 // try to elminate these global variables in your project, these are here just to start.
-var playersGuess,
-    winningNumber = generateWinningNumber();
-
+var playersGuess;
+var winningNumber = generateWinningNumber();
 var numGuesses = 0;
 var guessArray = [];
 
@@ -24,8 +23,31 @@ function playersGuessSubmission(){
 // Determine if the next guess should be a lower or higher number
 
 function lowerOrHigher(){
-	// add code here
+	if (playersGuess > winningNumber && (Math.abs(winningNumber - playersGuess) < 5)) { // within 5 digits higher
+		console.log("Your guess is higher than the number, you are wihin 5 digits");
+	} else if (playersGuess < winningNumber && (Math.abs(winningNumber - playersGuess) < 5)) // within 5 lower
+		console.log("Your guess is lower then the number, you are wihin 5 digits");
+	else if (playersGuess > winningNumber && (Math.abs(winningNumber - playersGuess) < 10)) { // within 10 digits higher
+		console.log("Your guess is higher then the number, you are wihin 10 digits");
+	} else if (playersGuess < winningNumber && (Math.abs(winningNumber - playersGuess) < 10)) { // within 10 digits lower
+		console.log("Your guess is lower then the number, you are wihin 10 digits");
+	} else if (playersGuess < winningNumber && (Math.abs(winningNumber - playersGuess) < 20)) { // within 20 digits lower
+		console.log("Your guess is less then the number, you are wihin 20 digits");
+	} else if (playersGuess > winningNumber && (Math.abs(winningNumber - playersGuess) < 20)) { // within 20 digits higher
+		console.log("Your guess is higher then the number, you are wihin 20 digits");
+	} else if (playersGuess < winningNumber && (Math.abs(winningNumber - playersGuess) > 20)) { // more then 20 digits lower
+		console.log("Your guess is lower then the number, more than 20 digits");
+	} else if (playersGuess > winningNumber && (Math.abs(winningNumber - playersGuess) > 20)) { //  more then 20 digits higher
+		console.log("Your guess is higher then the number, more than 20 digits");
+	}
 }
+
+
+function message(){
+
+}
+
+
 
 // Check if the Player's Guess is the winning number 
 
