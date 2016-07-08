21f7fd810fad9fd44d7535782ce06b44faf2589f
George Smith-Sweeper <george.smsweeper@gmail.com>
2016-07-08 01:13:12 -0400

    finished game

--- guessingGame.js
+++ guessingGame.js
@@ -1,14 +1,14 @@
   /* **** Global Variables **** */
 // try to elminate these global variables in your project, these are here just to start.
-var playersGuess;
-var winningNumber = generateWinningNumber();
-var numGuesses = 0;
-var guessArray = [];
-
+(function(){
+	var playersGuess;
+	var winningNumber = generateWinningNumber();
+	var numGuesses = 0;
+	var guessArray = [];
+	var guessesRemaining = 5;
 /* **** Guessing Game Functions **** */
 
 // Generate the Winning Number
-
 function generateWinningNumber(){
 	return Math.floor(Math.random() * (100 - 1) + 1);
 };
@@ -20,8 +20,8 @@ function playersGuessSubmission(){
 	$(document.getElementById("playersGuessInput")).val("");
 };
 
-// Determine if the next guess should be a lower or higher number
 
+// Determine if the next guess should be a lower or higher number
 function lowerOrHigher(){
 	function directionSwitch() {
 		var direction = "";
@@ -33,12 +33,11 @@ function lowerOrHigher(){
 			return direction;
 		}
 	}
-
 	if (Math.abs(winningNumber - playersGuess) <= 5) { // within 5 digits higher
 		return directionSwitch() + "you are within 5 digits.";
-	} else if (Math.abs(winningNumber - playersGuess) <= 5) // within 5 lower
+	} else if (Math.abs(winningNumber - playersGuess) <= 5) {// within 5 lower
 		return directionSwitch() + "you are within 5 digits.";
-	  else if (Math.abs(winningNumber - playersGuess) <= 10) { // within 10 digits higher
+	}else if (Math.abs(winningNumber - playersGuess) <= 10) { // within 10 digits higher
 		return directionSwitch() + "you are within 10 digits.";
 	} else if (Math.abs(winningNumber - playersGuess) <= 10) { // within 10 digits lower
 		return directionSwitch() + "you are within 10 digits.";
@@ -53,32 +52,58 @@ function lowerOrHigher(){
 	}
 }
 
-// message for the DOM 
 
+// message for the DOM
 function guessMessage(){
 	$("#updates").text(lowerOrHigher());
 }
 
-// Check if the Player's Guess is the winning number 
 
-function checkGuess(){
-	if (playersGuess === winningNumber) {
-		$("#updates").text("You guessed the number!");
-	} else { 
-		if (guessArray.indexOf(playersGuess) !== -1) {  //check if player already added 
-			$("#updates").text("You entered " + playersGuess + " already, guess again"); 
-		} else {
-			numGuesses++; // increment guesses
-			guessArray.push(playersGuess); // add guesses to array
-			guessMessage(); 
+// Game Over Lose
+function gameOver() {
+	$(".jumbotron").css("background-image", "url(gameOver.gif)");
+	$("#mainTitle").text("You Lose").css("color", "white");
+	$("p").text("");
+	$("#playersGuessInput, #hintBtn, #submitBtn").prop("disabled", true); // disable input, hint, and submit
+}
+
+
+// Game Over Winner
+function youWin() {
+	$(".jumbotron").css("background-image", "url(fireworks.gif)")
+	$("#mainTitle").text("You Win").css("color", "white");
+	$("p").text("");
+	$("#playersGuessInput, #hintBtn, #submitBtn").prop("disabled", true);
+}
+
+
+// Check if the Player's Guess is the winning number 
+function checkGuess(){ 
+	//var guessesRemaining = 5;
+	if (isNaN(playersGuess) || playersGuess < 1 || playersGuess > 100) { // NaN, less than one, greater than 100
+		$("#updates").text("Invalid entry, try again");
+	} else {
+		if (playersGuess === winningNumber) {
+			youWin();
+		} else { 
+			if (guessArray.indexOf(playersGuess) !== -1) {  // check if players guess is duplicate 
+				$("#updates").text("You entered " + playersGuess + " already, guess again"); 
+			} else {
+				numGuesses++; // increment guesses
+				guessesRemaining--; // decrement guesses remaining
+				$("p").append("p").text("You have " + guessesRemaining + " guesses left."); //remaining guesses countdown
+				guessArray.push(playersGuess); // add guesses to array
+				guessMessage(); 
+				if (numGuesses >= 5) {
+					gameOver();
+				}
+			}
 		}
 	}
 }
-	
 
 
 // Create a provide hint button that provides additional clues to the "Player"
-
 function provideHint(){
 	function randomNumHintGen(){ //create random number each time function is called
 		return Math.floor(Math.random() * (100 - 1) + 1);
@@ -104,14 +129,9 @@ function provideHint(){
 	return hintArray;
 }
 
-// Allow the "Player" to Play Again
-
-function playAgain(){
-	document.location.reload(true);
-}
-
 
 /* **** Event Listeners/Handlers ****  */
+
 
 // listen for enter key press on input field
 $("#playersGuessInput").on("keyup", function(enter) {
@@ -122,12 +142,18 @@ $("#playersGuessInput").on("keyup", function(enter) {
 	}
 });
 
+//submit button
+$("#submitBtn").click(function() {
+	playersGuessSubmission();
+	checkGuess();
+	lowerOrHigher();
+});
 
-
+// hint button functionality
 $("#hintBtn").click(function() {
 	if (numGuesses === 0) { 
 		// response for pressing hint without guessing
-		$("#updates").text("You haven't event guessed! No hint for you!")
+		$("#updates").text("You haven't even guessed! No hint for you!")
 	} else {
 		provideHint();
 		// show hintArray 
@@ -135,16 +161,16 @@ $("#hintBtn").click(function() {
 	}
 });
 
+
 // reset the page if "playAgain" button is clicked
 $("#playAgain").click(function() {
-	playAgain();
+	document.location.reload(true);
 });
 
 
-
+// generate number when the DOM is ready
 $(document).ready(winningNumber);
-
-
+}());
 
 
 
